3bb649be0c410bf9a3cf102511bd839d766b7f0b
Kristi Ingco <kristiana.d.ingco@gmail.com>
2025-07-08 17:11:46 -0700

    Use assertions

--- conftest.py
+++ conftest.py
@@ -5,7 +5,13 @@ def prework():
 
 def postwork():
     print("postwork")
-    
+
 @pytest.fixture(scope="function")
 def setup():
     prework()
+    return True
+
+@pytest.fixture(scope="function")
+def post_test():
+    postwork()
+    return True
--- test_pytest_validation.py
+++ test_pytest_validation.py
@@ -1,2 +1,4 @@
-def test_initial_check(setup):
+def test_initial_check(setup, post_test):
     print("test_initial_check")
+    assert setup
+    assert post_test
